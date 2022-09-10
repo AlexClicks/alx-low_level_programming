77a40580dafb5bc3bf2aa37d621f3d3b1f9ff01d
Alex <ugwuanyicornelius53@gmail.com>
2022-09-10 05:22:45 -0700

    Made some modification on 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,8 +8,12 @@
 int main(void)
 {
 	char a;
-	for (a = 'a'; a <= 'z'; a++)
-		putchar(a);
-		putchar('\n');
-		return(0);
+ for (a = 'a'; a <= 'z'; a++)
+ {
+	 putchar(a);
+	 putchar('\n');
+
+ }
+
+ return (0);
 }
