57bfe7fe04b0191db989deeeeba4c4fd2c8b1c87
Alex <ugwuanyicornelius53@gmail.com>
2022-09-09 22:02:54 -0700

    Added the file; 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry points
+ *
+ * Return: Always returns 0
+ *
+ */
+int main(void)
+{
+	char a;
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+		putchar('\n');
+		return(0);
+	}
+}
