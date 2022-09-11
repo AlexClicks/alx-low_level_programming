4876bb8b5882235edc764c6e1ee44c2f63dd9c0a
Alex <ugwuanyicornelius53@gmail.com>
2022-09-10 23:29:47 -0700

    added the file; 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always Return 0
+ *
+ */
+int main(void)
+{
+	int a;
+
+	for (; a <= 9; a++)
+	{
+		putchar('0' + a);
+	}
+	printf("\n");
+	return (0);
+}
+
