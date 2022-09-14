8fc1516de982588052f484f96c70f8d82f787504
Alex <ugwuanyicornelius53@gmail.com>
2022-09-14 08:43:05 -0700

    function for times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * times_table - times table functions
- * Return: Returns the times table of numbers
+ * Return: Returns the times table of numbers up to 9
  */
 void times_table(void)
 {
@@ -27,7 +27,7 @@ void times_table(void)
 			}
 			else
 			{
-				_pitchar(',');
+				_putchar(',');
 				_putchar(' ');
 				_putchar(p / 10 + '0');
 				_putchar(p % 10 + '0');
