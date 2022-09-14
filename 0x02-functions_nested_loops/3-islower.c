412684751b821a5d7d2290e4a22749608ef6b057
Alex <ugwuanyicornelius53@gmail.com>
2022-09-13 21:47:40 -0700

    Another modification made on this file

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- *
  * _islower - main function
  *
  * #c: charter to compare
@@ -8,7 +7,7 @@
  * Return: 1 if true, 0 if false
  *
  */
-int _islower (int c)
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
