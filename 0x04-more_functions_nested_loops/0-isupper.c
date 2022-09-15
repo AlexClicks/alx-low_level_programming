de2800be3f64a741ed61cb0d2ee30a0657d0890f
Alex <ugwuanyicornelius53@gmail.com>
2022-09-15 13:12:57 -0700

    function that print charactiers in lower case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *  _islower - Entry point
+ *
+ * @c: Character to be checked
+ *
+ * Return: Returns the character being checked
+ *
+ */
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
