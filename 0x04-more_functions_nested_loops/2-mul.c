2f1a3eb4660a000cbed0e340e083f390568a3fa8
Alex <ugwuanyicornelius53@gmail.com>
2022-09-16 01:25:42 -0700

    Function that return the sum of numbers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,15 @@
 #include "main.h"
+
+
+
 /**
  * mul: main funtion
- *
  * @a: character to be sued
- *
  * @b:  Character to be used
- * 
  * Return: Return the sum of the two characters
- *
  */
+
+
 int mul(int a, int b)
 {
 	return (a * b);
---
+++ 0x04-more_functions_nested_loops/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+        int i;
+        int j;
+
+        for (j = 0; j <= 9; j++)
+        {
+                for (i = 0; i <= 14; i++)                     {
+                        printf("%d",i);
+                }
+                printf("\n");
+        }
+        printf("\n");
+}
