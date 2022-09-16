cc2ce32f64b7f9141305284bb98acd7d4ca6e450
Alex <ugwuanyicornelius53@gmail.com>
2022-09-16 15:25:58 -0700

    function that will print whetther a code is positve or negetive

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - test function that print if an integer is positive or negetive
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
