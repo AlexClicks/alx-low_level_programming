a37ec3ed7d55d0e5db83cd018381f82b7349d0a9
Alex <ugwuanyicornelius53@gmail.com>
2022-09-16 15:37:00 -0700

    function that returns wheather a number is positive or negetive

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,15 @@
 #include "main.h"
 
-
 /**
  * main - test function that print if an integer is positive or negetive
- * @i: character that stores that number to be printed
  * Return: 0
  */
-
 
 int main(void)
 {
 	int i;
-	i = 0;
 
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
