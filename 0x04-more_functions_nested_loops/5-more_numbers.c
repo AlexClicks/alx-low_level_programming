87405e2f63ba8747a8fa546a405d10d482dbf7b7
Alex <ugwuanyicornelius53@gmail.com>
2022-09-16 02:50:44 -0700

    Function that print a range of numbers 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,17 +3,22 @@
  * more_numbers - Function to get the 10 multioles
  *
  * Return: Returns 0
+ *
  */
 void more_numbers(void)
 {
-	char i;
-	char j;
+	int i;
+	int j;
 
 	for (j = 0; j <= 9; j++)
 	{
-		for (i = '0'; i <= '14'; i++)
+		for (i = 0; i <= 14; i++)
 		{
-			_putchar(i);
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
 	}
