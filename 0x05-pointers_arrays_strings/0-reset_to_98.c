32634d719fde31f3c9297ec17d4356afbe3bb9dc
Alex <ugwuanyicornelius53@gmail.com>
2022-09-19 15:03:31 -0700

    Function that convert any point to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,9 @@
 #include "main.h"
 
 /**
- *
  * reset_to_98 - main function
- *
  * @n: Character to be passed as an arguent.
- *
- * Return: Returns the pointer value.
+ * Description: Returns the pointer value.
  */
 void reset_to_98(int *n)
 {
