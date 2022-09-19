f784787f00a84f5dd33dc32632068aa0eac2273e
Alex <ugwuanyicornelius53@gmail.com>
2022-09-19 14:31:23 -0700

    Character that update the value of a pointer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *
+ * reset_to_98 - main function
+ *
+ * @n: Character to be passed as an arguent.
+ *
+ * Return: Returns the pointer value.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+fndef _MAIN_
+#define _MAIN_
+/** reset_to_98 - Change the value of a pointer
+ *
+ * Return: Returns the value of the pointer
+ */
+void reset_to_98(int *n);
+#endif
