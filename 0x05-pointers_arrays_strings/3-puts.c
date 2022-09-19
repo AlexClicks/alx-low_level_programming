b641630e5f8f495259a9b039155451c877597393
Alex <ugwuanyicornelius53@gmail.com>
2022-09-19 15:50:41 -0700

    Function that print the length with new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - main functions
+ * @str: a pointer given by the main function
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int m = 0;
+
+	while(str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar(10);
+}
