21ccbfaf16c84735a3744eb95825a946e0802093
Alex <ugwuanyicornelius53@gmail.com>
2022-09-19 20:57:51 -0700

    function that reverse a character

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - prints spring in reverse
+ * @s: character that given by main
+ * Return: Always 0
+ */
+void rev_string(char *s)
+{
+	int counter = 0;
+	int m = 0;
+	
+	char swap;
+
+	while (s[counter] != '\0')
+	{
+		counter++;
+	}
+	counter -= 1;
+
+	while (counter > i)
+	{
+		swap = s[i];
+		s[i] = s[counter];
+		s[counter] = swap;
+		counter--;
+		i++;
+	}
+}
