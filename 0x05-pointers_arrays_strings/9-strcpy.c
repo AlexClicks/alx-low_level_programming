86629e585f3347512c6472b0d434cce0eeb7f090
Alex <ugwuanyicornelius53@gmail.com>
2022-09-19 21:57:42 -0700

    Function that copies one file to another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcpy - copies the string pointed to src including \o
+ * @src: a pointer
+ * @dest: a pointer
+ * Return: pointer to the dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int length;
+	int i;
+
+	for (length = 0; str[length] != '\0'; length++)
+	{
+	}
+
+	for (i = 0; i <= length; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
