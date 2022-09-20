f9c6d1f6af36b8c22a7c44a995218fa524487922
Alex <ugwuanyicornelius53@gmail.com>
2022-09-19 20:31:15 -0700

    Function that prints number in reverse followed by newline

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a stting in the reverse 
+ * @s: point obtained from the main function
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	int counter = 0;
+
+	while (s[counter] != '\0')
+	{
+		counter++;
+	}
+
+	while (counter)
+	{
+		_putchar(s[counter -1]);
+		counter--;
+	}
+	_putchar(10);
+}
+
+
