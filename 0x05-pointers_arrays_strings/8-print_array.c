8cb91989342905c9524ee671ab7b063d1c326818
Alex <ugwuanyicornelius53@gmail.com>
2022-09-19 22:38:03 -0700

    function that print an aray of characters

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_array - print array of length l
+ * @a: a pointer given by a
+ * @n: a pointer given by main function
+ * Return: Returns the array printed
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i ++)
+	{
+		printf("%d", a[i]);
+
+		if (i < n - 1)
+		{
+			printf(",");
+		}
+	}
+
+	printf("\n");
+}
