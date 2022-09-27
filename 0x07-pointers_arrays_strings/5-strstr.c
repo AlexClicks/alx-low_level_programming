ae4937f9b8f1d8962a1becba6ad08f514abbb458
Alex <ugwuanyicornelius53@gmail.com>
2022-09-26 21:43:36 -0700

    Function that searches for a substrings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strstr - Function that searches for a substring in a string
+ * @haystack: The string to be worked on
+ * @needle: The sub string to be searched for
+ * Return: Returns the value of the substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int a;
+	int b;
+
+	if (needle[0] == '\0')
+	{
+		return (haystack);
+	}
+	for (a = 0; haystack[a] != '\0'; a++)
+	{
+		if (haystack[a] == needle[0])
+		{
+			for (b = 0; needle[b] != '\0';b++)
+			{
+				if (haystack[a + b] != needle[b])
+				{
+					break;
+				}
+			}
+			if (needle[b] == '\0')
+			{
+				return (haystack + a);
+			}
+		}
+	}
+	return ('\0');
+}
+
