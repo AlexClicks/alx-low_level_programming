54b029961f54f07f2beb45a1686eeb751ed140a6
Alex <ugwuanyicornelius53@gmail.com>
2022-09-26 22:24:53 -0700

    Function thay prints the sum of the two diagonal of squares

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagsums - Function that prints sum of two diagonal of a squar
+ * @a: Variable to store the length of string
+ * @size: Variable for the size of the square
+ * Return: Return the sum of the diagonal
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int b;
+	int diagO = 0;
+	int diagT = 0;
+
+	for (b = 0; b < size; b++)
+	{
+		diagO += a[(b * size) + b];
+		diagT += a[(size - 1) + ((size - 1) * b)];
+	}
+	printf("%d, %d\n", diagO, diagT);
+}
