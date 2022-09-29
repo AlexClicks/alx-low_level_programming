85a6db53d7b54f1189198d7a328b2c66d4839225
Alex <ugwuanyicornelius53@gmail.com>
2022-09-28 22:38:13 -0700

    Function that check wheather a number is a palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * length - helps to do it
+ * @s: string to be worked
+ * Return: Rerurn string length
+ */
+int length(char *s)
+{
+	int i = 0;
+
+	if(*s)
+	{
+		i = i + length(s +1);
+		return (i += 1);
+	}
+	return (0);
+}
+
+/**
+ * helper2 - Helps more
+ * @i: interger for interation
+ * @s: string
+ * Return: int value
+ */
+
+int helper2(int i, char *s)
+{
+	if(*s)
+	{
+		if(*s != s[length(s) - i])
+		{
+			return(0);
+		}
+		else
+		{
+			return (helper2(i + 1, s + 1));
+		}
+	}
+	return (1);
+
+}
+
+/**
+ * is_palindrome - fuction that check if itbis palindrom
+ * @s: string to check
+ * Return: Return if it is palindrome
+ */
+int is_palindrome(char *s)
+{
+	int i = 1;
+
+	return (helper2(i, s));
+}
