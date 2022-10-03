9829f388603e76ad2ab486c48fec076f6ba981c2
Alex <ugwuanyicornelius53@gmail.com>
2022-10-02 21:23:36 -0700

    function that prints its name and other argument used to run it

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+
+ * main - Function that prints ists name and argument use to run it.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: Always o.
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
